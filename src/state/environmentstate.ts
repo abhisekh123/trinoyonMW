ec6820e8a7c321313d8dcfe375d48f59140063c9
abhisekh_biswas <avi.priceless@gmail.com>
2020-02-13 16:31:47 +0530

    callback url rename.

--- src/state/environmentstate.ts
+++ src/state/environmentstate.ts
@@ -9,7 +9,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '183311358526115', // your App ID
         'clientSecret'  : '75653a289a881533ff00a4ce330db23a', // your App Secret
-        'callbackURL'   : 'https://trinoyon/auth/facebook/callback',
+        'callbackURL'   : 'https://trinoyon.com/auth/facebook/callback',
         // 'callbackURL'   : 'http://localhost:8080/auth/facebook/callback',
         'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
         'profileFields' : ['id', 'email', 'name'], // For requesting permissions from Facebook API,
