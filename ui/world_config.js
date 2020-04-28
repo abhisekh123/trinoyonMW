5fccd78385e51f87bad96a9771bb21921a3d8530
abhisekh_biswas <avi.priceless@gmail.com>
2020-04-28 12:47:42 +0530

    changed game duration

--- ui/world_config.js
+++ ui/world_config.js
@@ -55,7 +55,7 @@ var worldItems = {
     refreshWorldInterval: 500, // refreshWorld() should run once every interval.
     refreshWorldPerIntervalUI: 10,
     processActionResolution: 200, // for each refreshWorld() delta time will be broken into interval of this.
-    matchMaxTimeDuration: 1 * 60 * 1000, // 5 minutes
+    matchMaxTimeDuration: 6 * 60 * 1000, // 6 minutes
     // topTeamCaracters:[[44,8], [46, 6], [42, 6]],
     // bottomTeamCaracters:[[44,80], [46, 82], [42, 82]],
     // top team = 1
